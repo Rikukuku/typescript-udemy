07d1d4c24803e214ac1dddcd9b60609fef51100e
Rikukuku <ricky_gland821_0714@yahoo.co.jp>
2020-02-13 14:37:20 +0900

    Add setting

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let str:string = 'Hello World!!'
+console.log(str)
+
+const obj:object = { message: "Hello ts-node"}
+console.log(obj)
+
+const num:number = 2
+console.log(num)
+
+const two:number = 2
+console.log(num)
+
+const magic = ():void => {
+  console.log('test')
+}
